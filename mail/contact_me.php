8f9b97cbadedf06908833de44957eceb9c440c55
adiorio6588 <diorio.anthony11b@gmail.com>
2020-04-13 20:44:04 -0700

    Started linking the buttons, and made new staff bio pages.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 
-$to = 'diorio_anthony@codediver.dev';
+$to = 'diorio_anthony@yahoo.com';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@diorio_anthony@codediver.dev\n"; 
+$headers = "From: noreply@diorio_anthony@yahoo.com\n"; 
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
